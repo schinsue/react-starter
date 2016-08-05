eaed52c016d2f245830297e938f9a1bf6a654343
Sergio Chin-Sue <sergio.chin-sue@finalist.com>
2016-08-05 16:36:40 +0200

    Added basic react structures as example

---
+++ src/components/common/Header.js
@@ -0,0 +1,16 @@
+// Stateless component as an example
+
+import React , {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+    </nav>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// Some ES6 functionality can't be transpiled without polyfill, only import the functionality you need!
+import 'babel-polyfill';
+
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+
+// You won't need this if you're gonna use SASS/LESS
+import './styles/styles.css'; // Webpack imports CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render (
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
